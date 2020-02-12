a965c9211b67f4be3c2a59e9502d246fe268148c
Doop2fab <46304917+Doop2fab@users.noreply.github.com>
2020-02-11 20:26:46 -0600

    Update datehw.java

--- datehw.java
+++ datehw.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
-public class Date{
+public class datehw{
     public static void main(String[] args){
         String date;
+        int month, day, year;
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please enter a date in the format mm/dd/yyyy");
         date = keyboard.next();
-        
+        String[] splitDate = date.split("/");
+
+        month = Integer.parseInt(splitDate[0]);
+        day = Integer.parseInt(splitDate[1]);
+        year = Integer.parseInt(splitDate[2]);
+
+        System.out.println(month);
+        System.out.println(day);
+        System.out.println(year);
 
 
     }
