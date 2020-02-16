7a210ec7883ec842e337e190bcd87b8f8557828c
Doop2fab <46304917+Doop2fab@users.noreply.github.com>
2020-02-15 23:10:02 -0600

    Update solver.java

--- solver.java
+++ solver.java
@@ -1,11 +1,11 @@
 public class solver {
     static int address = 1000;
 
-    public static boolean isTrue(int address) {
-        int first = address / 1000;
-        int second = (address / 100) - (10 * first);
-        int third = (address / 10) - (10 * second) - (100 * first);
-        int fourth = address - (10 * third) - (100 * second) - (1000 * first);
+    public static boolean isTrue(int newAddress) {
+        int first = newAddress / 1000;
+        int second = (newAddress / 100) - (10 * first);
+        int third = (newAddress / 10) - (10 * second) - (100 * first);
+        int fourth = newAddress - (10 * third) - (100 * second) - (1000 * first);
         if (first == second || first == third || first == fourth || second == third || second == fourth
                 || third == fourth || first != (3 * third) || fourth % 2 == 0
                 || (first + second + third + fourth) != 27) {
