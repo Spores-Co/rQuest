0a89ed176ce944f3c32e47902c7a6363c123163a
Doop2fab <46304917+Doop2fab@users.noreply.github.com>
2020-05-09 19:28:04 -0500

    REEEEE FIX! PLEASE!

---
+++ rQuest/app/src/main/java/com/doopstudio/rquest/openShop.java
@@ -0,0 +1,14 @@
+package com.doopstudio.rquest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class openShop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_shop);
+    }
+}
